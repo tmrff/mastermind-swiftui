4c1ab16b3135c6555a4906fcfb78f925e42076a4
tmrff <thomasdroff@gmail.com>
2024-07-02 20:05:36 +1200

    initial commit

---
+++ Mastermind/MastermindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MastermindApp.swift
+//  Mastermind
+//
+//  Created by Thomas on 02/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MastermindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
