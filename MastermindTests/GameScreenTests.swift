8f3e79dc2f0fc2bb0bff36290e18c6224410f426
tmrff <thomasdroff@gmail.com>
2024-07-03 12:47:06 +1200

    move helpers down

--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -6,6 +6,18 @@ import SwiftUI
 
 final class GameScreenTests: XCTestCase {
     
+    func test_tappingCircleTurnsItOrange() throws {
+        var sut = GameScreen()
+        var color = try getColorOfGuess(try sut.inspect())
+        XCTAssertNotEqual(color, Color.orange, "Precondition")
+        
+        display(&sut) { view in
+            try view.find(viewWithId: "guess1").button().tap()
+            color = try self.getColorOfGuess(view)
+        }
+        XCTAssertEqual(color, Color.orange)
+    }
+    
     private func display(
         _ sut: inout GameScreen,
         using: @escaping ((InspectableView<ViewType.View<GameScreen>>) throws -> Void),
@@ -19,17 +31,5 @@ final class GameScreenTests: XCTestCase {
     
     private func getColorOfGuess<V: ViewInspector.KnownViewType>(_ view: InspectableView<V>) throws -> Color? {
         try view.asInspectableView().find(viewWithId: "guess1").button().labelView().shape().foregroundColor()
-    }
-    
-    func test_tappingCircleTurnsItOrange() throws {
-        var sut = GameScreen()
-        var color = try getColorOfGuess(try sut.inspect())
-        XCTAssertNotEqual(color, Color.orange, "Precondition")
-        
-        display(&sut) { view in
-            try view.find(viewWithId: "guess1").button().tap()
-            color = try self.getColorOfGuess(view)
-        }
-        XCTAssertEqual(color, Color.orange)
     }
 }
