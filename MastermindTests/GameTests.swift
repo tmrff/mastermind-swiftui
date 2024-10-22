ff077b9f314e97bf379c08d65d38a7e32d765a3f
Thomas <thomasdroff@gmail.com>
2024-10-22 22:41:03 +1300

    add gameHasNoSecretInitially test

---
+++ MastermindTests/GameTests.swift
@@ -0,0 +1,9 @@
+@testable import Mastermind
+import XCTest
+
+final class GameTests: XCTestCase {
+    func test_gameHasNoSecretInitially() throws {
+        let game = try Game(numberOfCodeChoices: 2, SecretMaker.createNull())
+        XCTAssertEqual(game.secret, [])
+    }
+}
