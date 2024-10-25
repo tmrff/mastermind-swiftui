d6da56cd59bdc74ec7c1973c6b1e020aecad8d5a
Thomas <thomasdroff@gmail.com>
2024-10-26 11:32:05 +1300

    hard-code around isGuessCorrect until we have secret length == options length

--- Mastermind/GameScreen.swift
+++ Mastermind/GameScreen.swift
@@ -18,7 +18,8 @@ struct GameScreen: TestableView {
             }
         }
         .inspectableSheet(isPresented: .constant(guess1 != nil)) {
-            if (game.isGuessCorrect([guess1!])) {
+//            if (game.isGuessCorrect([guess1!])) {
+            if (game.secret.code[0] == guess1) {
                 Text("You win!")
             } else {
                 Text("You lose!")
