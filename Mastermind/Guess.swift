f912b08c4e836f2c9e9e9b5761f5c1cf217bfa0f
Thomas <thomasdroff@gmail.com>
2024-10-28 21:08:40 +1300

    setGuessAt

--- Mastermind/Guess.swift
+++ Mastermind/Guess.swift
@@ -1,8 +1,12 @@
 import Foundation
 
-struct Guess {
-    let code: [CodeChoice?]
+class Guess {
+    var code: [CodeChoice?]
     init(secretSize: Int) {
         code = Array(repeating: nil, count: secretSize)
+    }
+    
+    func setGuessAt(_ index: Int, _ choice: CodeChoice) {
+        code[index] = choice
     }
 }
