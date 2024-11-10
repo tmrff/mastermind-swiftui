253380b45e721fd9588ef25b18246a652053db2b
Thomas <thomasdroff@gmail.com>
2024-11-10 21:32:41 +1300

    pass in secretSize explicitly

--- MastermindTests/GameScreenTests.swift
+++ MastermindTests/GameScreenTests.swift
@@ -5,7 +5,7 @@ import SwiftUI
 @testable import Mastermind
 
 extension Game {
-    fileprivate convenience init(numberOfCodeChoices: Int, secretSize: Int = 1) throws {
+    fileprivate convenience init(numberOfCodeChoices: Int, secretSize: Int) throws {
         try self.init(numberOfCodeChoices: numberOfCodeChoices, secretSize: secretSize, SecretMaker.createNull())
     }
 }
@@ -15,7 +15,7 @@ extension InspectableSheet: @retroactive PopupPresenter {}
 final class GameScreenTests: XCTestCase {
     
     @MainActor func test_displaysCodeChoicesBottomUp() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         let sut = GameScreen(game: game)
         
         let choice1 = try getCodeChoiceColor(sut.inspect(), 0)
@@ -26,14 +26,14 @@ final class GameScreenTests: XCTestCase {
     }
     
     @MainActor func test_showingGameScreen_setsGameSecret() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         _ = GameScreen(game: game)
         XCTAssertFalse(game.secret.code.isEmpty)
     }
     
     @MainActor
     func test_initialColorGuessIsUnselected() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         let sut = GameScreen(game: game)
         
         let color = try getColorOfGuess(try sut.inspect())
@@ -43,7 +43,7 @@ final class GameScreenTests: XCTestCase {
     
     @MainActor
     func test_tappingCodeChoiceSetsGuessColor() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         var sut = GameScreen(game: game)
         let codeChoice = game.codeChoice(0)
         var color: Color?
@@ -56,13 +56,13 @@ final class GameScreenTests: XCTestCase {
     }
     
     @MainActor func test_doesNotShowGameOverWhenCodeChoiceIsEmpty() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         let sut = GameScreen(game: game)
         XCTAssertThrowsError(try sut.inspect().find(ViewType.Sheet.self))
     }
     
     @MainActor func test_showsGameOverWhenCodeChoiceIsFilled() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         var sut = GameScreen(game: game)
         let codeChoice = game.codeChoice(0)
         
@@ -73,7 +73,7 @@ final class GameScreenTests: XCTestCase {
     }
     
     @MainActor func test_gameOverShowsYouLoseWhenGuessDoesNotMatchSecret_secretSize1() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         var sut = GameScreen(game: game)
         game.secret = Secret(code: [game.codeChoice(1)])
         let codeChoiceToTap = game.codeChoice(0)
@@ -88,7 +88,7 @@ final class GameScreenTests: XCTestCase {
     }
     
     @MainActor func test_gameOverShowsYouWinWhenGuessMatchesSecret_secretSize1() throws {
-        let game = try Game(numberOfCodeChoices: 2)
+        let game = try Game(numberOfCodeChoices: 2, secretSize: 1)
         var sut = GameScreen(game: game)
         game.secret = Secret(code: [game.codeChoice(1)])
         let codeChoiceToTap = game.secret.code[0]
