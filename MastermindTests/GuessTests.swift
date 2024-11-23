1c4e4640f00159e0f0204816b79301597a558a78
Thomas <thomasdroff@gmail.com>
2024-11-23 22:49:40 +1300

    extract makeSUT

--- MastermindTests/GuessTests.swift
+++ MastermindTests/GuessTests.swift
@@ -18,18 +18,18 @@ final class GuessTests: XCTestCase {
     }
     
     func test_createGuess_setsGuessSize() throws {
-        let sut = Guess(secretSize: 2)
+        let sut = makeSUT(secretSize: 2)
         XCTAssertEqual(sut[0], nil)
         XCTAssertEqual(sut[1], nil)
     }
     
     func test_size() throws {
-        let sut = Guess(secretSize: 4)
+        let sut = makeSUT(secretSize: 4)
         XCTAssertEqual(sut.size, 4)
     }
     
     func test_setAndGetAtIndices() throws {
-        let sut = Guess(secretSize: 2)
+        let sut = makeSUT(secretSize: 2)
         sut[0] = choice0
         sut[1] = choice1
         XCTAssertEqual(sut[0], choice0)
@@ -37,13 +37,17 @@ final class GuessTests: XCTestCase {
     }
     
     func test_isObservable_soThatViewsCanObserveChanges() throws {
-        let sut = Guess(secretSize: 2)
+        let sut = makeSUT(secretSize: 2)
         XCTAssertTrue(sut is Observable)
     }
     
     func test_isNotComplete_withEmptySlot() throws {
-        let sut = Guess(secretSize: 2)
+        let sut = makeSUT(secretSize: 2)
         sut[0] = choice0
         XCTAssertFalse(sut.isComplete)
+    }
+    
+    private func makeSUT(secretSize: Int) -> Guess {
+        Guess(secretSize: secretSize)
     }
 }
