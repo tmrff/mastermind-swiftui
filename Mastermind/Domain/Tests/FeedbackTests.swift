ebeda1bb5e74835f70d5b8db5b2046e32549f5af
Thomas <thomasdroff@gmail.com>
2024-12-23 22:04:01 +1300

    clean up test with helper makeGuess

--- Mastermind/Domain/Tests/FeedbackTests.swift
+++ Mastermind/Domain/Tests/FeedbackTests.swift
@@ -5,15 +5,20 @@ struct FeedbackTests {
     private let red = CodeChoice(color: .red, codeValue: 1)
     private let green = CodeChoice(color: .green, codeValue: 2)
     private let blue = CodeChoice(color: .blue, codeValue: 3)
-
+    
     @Test
     func noMatches() async throws {
         let evaluator = FeedbackEvaluator(Secret(code: [red, green]))
-        let guess = Guess(secretSize: 2)
-        guess.placeChoiceInNextSlot(blue)
-        guess.placeChoiceInNextSlot(blue)
-        let correctColorsFeedback: Feedback = evaluator.evaluate(guess)
+        let correctColorsFeedback = evaluator.evaluate(makeGuess(code: [blue, blue]))
         #expect(correctColorsFeedback.inCorrectPosition == 0)
         #expect(correctColorsFeedback.inWrongPosition == 0)
+    }
+    
+    private func makeGuess(code: [CodeChoice]) -> Guess {
+        let guess = Guess(secretSize: code.count)
+        for choice in code {
+            guess.placeChoiceInNextSlot(choice)
+        }
+        return guess
     }
 }
