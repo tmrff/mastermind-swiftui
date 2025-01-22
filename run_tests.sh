93b22c5844b0b260f88c066410d39f9fa472037c
Thomas <thomasdroff@gmail.com>
2025-01-22 17:12:43 +1300

    set build server simulator to iPhone 16 Pro

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 SCHEME='Mastermind'
-DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16'
+DESTINATION='platform=iOS Simulator,OS=latest,name=iPhone 16 Pro'
 
 set -o pipefail && xcodebuild test -scheme $SCHEME -sdk iphonesimulator -destination "$DESTINATION" CODE_SIGNING_ALLOWED='NO' | xcbeautify
