3b0f473ca7ad7ad8264b732906aa542567ca375f
Thomas <thomasdroff@gmail.com>
2025-02-01 19:57:10 +1300

    feedbackPegs beginning with hardcoded count

---
+++ Mastermind/Domain/Tests/FeedbackPegsTests.swift
@@ -0,0 +1,15 @@
+@testable import Mastermind
+import Testing
+
+final class FeedbackPegsTests: @unchecked Sendable {
+    private let red = CodeChoice(color: .red, codeValue: 1)
+    private let green = CodeChoice(color: .green, codeValue: 2)
+    private let blue = CodeChoice(color: .blue, codeValue: 3)
+    
+    @Test
+    func numberOfPegsIsCodeLength() async throws {
+        let secret = Secret(code: [red, green, blue])
+        let feedbackPegs = FeedbackPegs(secret: secret)
+        #expect(feedbackPegs.count == 3)
+    }
+}
