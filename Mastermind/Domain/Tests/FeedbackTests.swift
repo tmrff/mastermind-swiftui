dcc7d7d3cc6c9d1bf78f30aa58faf5e5a4748ac3
Thomas <thomasdroff@gmail.com>
2025-02-01 20:37:24 +1300

    use Secret, not FeedbackEvaluator

--- Mastermind/Domain/Tests/FeedbackTests.swift
+++ Mastermind/Domain/Tests/FeedbackTests.swift
@@ -7,7 +7,7 @@ struct FeedbackTests {
     
     @Test
     func correctPositionInFirstPeg() async throws {
-        let evaluator = FeedbackEvaluator(Secret(code: [red, green]))
+        let evaluator = Secret(code: [red, green])
         let correctColorsFeedback = evaluator.evaluate(makeGuess(code: [red, red]))
         #expect(correctColorsFeedback.inCorrectPosition == 1)
         #expect(correctColorsFeedback.inWrongPosition == 0)
@@ -15,7 +15,7 @@ struct FeedbackTests {
     
     @Test
     func correctPositionInSecondPeg() async throws {
-        let evaluator = FeedbackEvaluator(Secret(code: [red, green]))
+        let evaluator = Secret(code: [red, green])
         let correctColorsFeedback = evaluator.evaluate(makeGuess(code: [green, green]))
         #expect(correctColorsFeedback.inCorrectPosition == 1)
         #expect(correctColorsFeedback.inWrongPosition == 0)
@@ -23,7 +23,7 @@ struct FeedbackTests {
     
     @Test
     func wrongPosition() async throws {
-        let evaluator = FeedbackEvaluator(Secret(code: [red, green]))
+        let evaluator = Secret(code: [red, green])
         let correctColorsFeedback = evaluator.evaluate(makeGuess(code: [green, red]))
         #expect(correctColorsFeedback.inCorrectPosition == 0)
         #expect(correctColorsFeedback.inWrongPosition == 2)
