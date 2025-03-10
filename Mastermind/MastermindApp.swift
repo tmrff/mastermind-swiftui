51f651e65c223feb2aef1283d038d556bca82a97
Thomas <thomasdroff@gmail.com>
2025-03-10 21:26:20 +1300

    increase game to 4x4

--- Mastermind/MastermindApp.swift
+++ Mastermind/MastermindApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct MastermindApp: App {
     var body: some Scene {
         WindowGroup {
-            GameScreen(game: try! Game(numberOfCodeChoices: 2, secretSize: 2, SecretMaker()))
+            GameScreen(game: try! Game(numberOfCodeChoices: 4, secretSize: 4, SecretMaker()))
         }
     }
 }
