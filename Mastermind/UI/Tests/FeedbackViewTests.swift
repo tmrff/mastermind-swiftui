1bb128eb17fe142f93d11177c0a38dba191061c9
Thomas <thomasdroff@gmail.com>
2025-03-16 15:44:26 +1300

    test peg 2

--- Mastermind/UI/Tests/FeedbackViewTests.swift
+++ Mastermind/UI/Tests/FeedbackViewTests.swift
@@ -9,8 +9,10 @@ final class FeedbackViewTests: XCTestCase, Sendable {
         let sut = FeedbackView(feedbackPegs: [.empty, .empty, .empty, .empty])
         
         let peg1Color = try sut.inspect().pegColor(1)
+        let peg2Color = try sut.inspect().pegColor(2)
         
         XCTAssertEqual(peg1Color, Color.unselected)
+        XCTAssertEqual(peg2Color, Color.unselected)
     }
 }
 
