edf098aeb1f13486530c716f9e485dca2910b46f
Thomas <thomasdroff@gmail.com>
2025-03-16 14:52:58 +1300

    define peg1 accessor

--- Mastermind/UI/Tests/FeedbackViewTests.swift
+++ Mastermind/UI/Tests/FeedbackViewTests.swift
@@ -1,7 +1,14 @@
 @testable import Mastermind
+import ViewInspector
 import XCTest
 
 @MainActor
 final class FeedbackViewTests: XCTestCase, Sendable {
     
+}
+
+private extension InspectableView {
+    func peg1() throws -> InspectableView<ViewType.Shape> {
+        try find(viewWithAccessibilityIdentifier: "feedback1").shape()
+    }
 }
